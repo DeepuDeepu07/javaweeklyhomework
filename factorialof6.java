bb412f5b55c8c7d8e02c0df642640af7478c04c3
deepika <deepikareddy.kandula@gmail.com>
2026-07-14 20:55:19 +0530

    added factorial

---
+++ factorialof6.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Program_factorialof6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int factorial=1;
+		int i=6;
+		for(i=6;i>=1;i--)
+		{
+			factorial=factorial*i;
+		}
+		System.out.println( "factorial of 6:"+factorial);
+	}
+		
+}
